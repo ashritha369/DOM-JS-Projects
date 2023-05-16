160f6b776b6b038e9ed0853bce2ceb5f3e668699
ashritha369 <ashrithagowda666@gmail.com>
2023-05-16 05:37:57 +0530

    working on refractoring the code

--- 2. Rock-Paper-Scissors-Game/refractoredCode.js
+++ 2. Rock-Paper-Scissors-Game/refractoredCode.js
@@ -8,35 +8,67 @@ const buttonRock = document.getElementById("rock");
 const buttonPaper = document.getElementById("paper");
 const buttonScissor = document.getElementById("scissor");
 
-// Define game logic
-const gameRules = {
-  ROCK: { beats: ["SCISSOR", "PAPER"], message: "PLAYER 1 WON" },
-  PAPER: { beats: ["ROCK", "SCISSOR"], message: "PLAYER 2 WON" },
-  SCISSOR: { beats: ["PAPER"], message: "PLAYER 1 WON" },
-  SCISSOR: { beats: ["ROCK"], message: "PLAYER 2 WON" },
-};
-
 // Initialize game state
 let playerCount1 = 0;
-let playerCount2 = 0;
+let playerCount2;
 
-// Handle player 2's random values
-function handlePlayer2RandomValues() {
-  const randomOption = getRandomOption();
-  player2.textContent = randomOption;
-  randomValue = randomOption;
+// Add event listeners
+const randomArray = ["ROCK", "PAPER", "SCISSOR"];
+let player1Value = "";
+let computerValue = "";
+
+function checkWinner(player1value, computerValue) {
+  if (player1value === computerValue) {
+    return "DRAW";
+  }
+
+  if (player1Value === "ROCK") {
+    if (computerValue === "PAPER") {
+      return "PLAYER 1 WINS!";
+    } else {
+      return "COMPUTER(PLAYER 2) WINS!";
+    }
+  }
+
+  if (player1Value === "PAPER") {
+    if (computerValue === "SCISSOR") {
+      return "COMPUTER(PLAYER 2) WINS!";
+    } else {
+      return "PLAYER 1 WINS!";
+    }
+  }
+
+  if (player1Value === "SCISSOR") {
+    if (computerValue === "PAPER") {
+      return "PLAYER 1 WINS!";
+    } else {
+      return "COMPUTER(PLAYER 2) WINS!";
+    }
+  }
 }
-
 // Handle player 1's button clicks
-function handleButton(clickedButton) {
-  clickedButtonValue = clickedButton;
-  player1.textContent = clickedButtonValue;
+function playGame(player1Value) {
+  player1.textContent = player1Value;
 
-  const result = getResult(clickedButtonValue, randomValue);
+  handlePlayer2ComputercomputerValues();
+  // Handle player 2's - Computer  random values
+  function handlePlayer2ComputercomputerValues() {
+    const randomOption = getRandomOption();
+    // Helper functions
+    function getRandomOption() {
+      const randomIndex = Math.floor(Math.random() * randomArray.length);
+      return randomArray[randomIndex];
+    }
+    player2.textContent = randomOption;
+    computerValue = randomOption;
+  }
+
+  const result = checkWinner(player1Value, computerValue);
+
   if (result === "DRAW") {
     displayWinner.textContent = "NO PLAYER WON";
   } else {
-    if (result === "PLAYER 1 WON") {
+    if (result === "PLAYER 1 WINS!") {
       playerCount1++;
       player1Score.textContent = playerCount1;
     } else {
@@ -47,42 +79,12 @@ function handleButton(clickedButton) {
   }
 }
 
-// Helper functions
-function getRandomOption() {
-  const randomIndex = Math.floor(Math.random() * randomArray.length);
-  return randomArray[randomIndex];
-}
-
-function getResult(option1, option2) {
-  if (option1 === option2) {
-    return "DRAW";
-  } else if (
-    gameRules[option1].beats[0] === option2 ||
-    gameRules[option1].beats[1] === option2
-  ) {
-    return gameRules[option1].message;
-  } else if (gameRules[option1].beats[0] === option2) {
-    return gameRules[option1].message;
-  } else {
-    return gameRules[option2].message;
-  }
-}
-
-// Add event listeners
-const randomArray = ["ROCK", "PAPER", "SCISSOR"];
-let clickedButtonValue = "";
-let randomValue = "";
-
-buttonRock.addEventListener("click", handlePlayer2RandomValues);
-buttonPaper.addEventListener("click", handlePlayer2RandomValues);
-buttonScissor.addEventListener("click", handlePlayer2RandomValues);
-
 buttonRock.addEventListener("click", function () {
-  handleButton("ROCK");
-});
-buttonScissor.addEventListener("click", function () {
-  handleButton("SCISSOR");
+  playGame("ROCK");
 });
 buttonPaper.addEventListener("click", function () {
-  handleButton("PAPER");
+  playGame("PAPER");
+});
+buttonScissor.addEventListener("click", function () {
+  playGame("SCISSOR");
 });
