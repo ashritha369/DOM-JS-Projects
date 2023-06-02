23521e2304a12d636f8fbbc1fb50dff7eaa04728
ashritha369 <ashrithagowda666@gmail.com>
2023-06-02 05:31:37 +0530

    added pseudo logic

--- 3.Hangman-Game-Word-Guessing/script.js
+++ 3.Hangman-Game-Word-Guessing/script.js
@@ -1,34 +1,45 @@
 let buttons = document.querySelectorAll(".btn");
 console.log(buttons);
 let wordSetter = "ASHI";
-
 let wordSetterArray = wordSetter.split("");
-
 let player2SelectionArray = [];
-
 let wrongCount = 0;
 let gameOverCount = 10;
 let clickedLetter;
+
 for (let i = 0; i < buttons.length; i++) {
   buttons[i].addEventListener("click", buttonClick);
 }
-function buttonClick() {
-  // console.log(`button clicked ${buttons[i].id}`);
-  //this= buttons[i]
-  clickedLetter = this.id;
-  //   console.log(clickedLetter);
-}
-console.log(clickedLetter);
+/*
+["A","S","H","I"]
 
-for (let i = 0; i < wordSetterArray.length; i++) {
-  while (wrongCount < gameOverCount) {
-    if (clickedLetter === wordSetterArray[i]) {
-      console.log(`Matched letter ${i} with ${clickedLetter}`);
-      player2SelectionArray.push(clickedLetter);
-    } else {
-      console.log("Not Matched try again");
-      wrongCount++;
-    }
+YES CONDITION
+-> i=0, first position check for A wordseter with  CLICKED ONE
+A==B?
+no;
+wrong count increase;
+if wrong count==gameover count
+game over
+go back and check for 1st postion only until wrong count==gameover count
+
+
+NO CONDITION
+--> for i=0;
+A==A?
+yes;
+push the clicked one to new Array;
+i++;
+
+  */
+function buttonClick() {
+  clickedLetter = this.id; //this= buttons[i]
+  check(clickedLetter);
+
+  if (wordSetterArray[i] === clickedLetter) {
   }
-  console.log("GAME OVER");
+
+  if (wordSetterArray[i] !== clickedLetter) {
+  }
 }
+
+function check(clickedLetter) {}
