68ec85b339fe41362fc62b42efdde91a206bba24
ashritha369 <ashrithagowda666@gmail.com>
2023-06-05 05:46:38 +0530

    working on logic

--- 3.Hangman-Game-Word-Guessing/script.js
+++ 3.Hangman-Game-Word-Guessing/script.js
@@ -7,39 +7,33 @@ let wrongCount = 0;
 let gameOverCount = 10;
 let clickedLetter;
 
-for (let i = 0; i < buttons.length; i++) {
-  buttons[i].addEventListener("click", buttonClick);
+/*for (let i = 0; i < buttons.length; i++) {
+  function check() {
+    // console.log(i);
+    buttons[i].addEventListener("click", buttonClick);
+    console.log(buttons[i].innerHTML);
+    if (wordSetterArray[0] === buttons[i].innerHTML) {
+      console.log(`Correct letter`);
+    } else {
+      console.log(`Wrong Letter`);
+    }
+  }
 }
-/*
-["A","S","H","I"]
-
-YES CONDITION
--> i=0, first position check for A wordseter with  CLICKED ONE
-A==B?
-no;
-wrong count increase;
-if wrong count==gameover count
-game over
-go back and check for 1st postion only until wrong count==gameover count
-
-
-NO CONDITION
---> for i=0;
-A==A?
-yes;
-push the clicked one to new Array;
-i++;
-
-  */
 function buttonClick() {
-  clickedLetter = this.id; //this= buttons[i]
-  check(clickedLetter);
-
-  if (wordSetterArray[i] === clickedLetter) {
-  }
-
-  if (wordSetterArray[i] !== clickedLetter) {
-  }
+  check();
+}
+*/
+function check(index) {
+  return function () {
+    console.log(buttons[index].innerHTML);
+    if (wordSetterArray[0] === buttons[index].innerHTML) {
+      console.log("Correct letter");
+    } else {
+      console.log("Wrong letter");
+    }
+  };
 }
 
-function check(clickedLetter) {}
+for (let i = 0; i < buttons.length; i++) {
+  buttons[i].addEventListener("click", check(i));
+}
